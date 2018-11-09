7026c3e8b3a58096da8e00f2cb6b82871e5d9be6
Ben Waugh <b.waugh@ucl.ac.uk>
2018-11-09 14:23:58 +0000

    Plot lepton mass distribution

--- dimuon.py
+++ dimuon.py
@@ -15,6 +15,19 @@ def hist_lep_n(data):
         hNumLeptons.Fill(data.lep_n)
     return hNumLeptons
 
+def hist_lep_pt(data):
+    '''
+    Creates a histogram of lepton pT in the data.
+    '''
+    hist = TH1F("lep_pt","Lepton pT",100,0,200)
+    for i_event in range(1000):
+        data.GetEntry(i_event)
+        lep_n = data.lep_n
+        for i_lep in range(lep_n):
+            pt = data.lep_pt[i_lep]
+            hist.Fill(pt*0.001)      # Convert pT from MeV to GeV
+    return hist
+
 def hist_dimuon_mass(data):
     hist = TH1F("mass","Dimuon mass",100,0,200)
     for i_event in range(1000):
@@ -29,6 +42,11 @@ if __name__ == '__main__':
 
     hNumLeptons = hist_lep_n(data)
     hNumLeptons.Draw()
+
+    raw_input("Press enter to see next histogram.")
+
+    hLepPt = hist_lep_pt(data)
+    hLepPt.Draw()
 
     raw_input("Press enter to see next histogram.")
 
