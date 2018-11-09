fa1d60577764ba1c7e746a94d70e2b2fad3612bf
Ben Waugh <b.waugh@ucl.ac.uk>
2018-11-09 12:37:49 +0000

    Refactoring: function to fill histogram, and fix memory bug

--- dimuon.py
+++ dimuon.py
@@ -1,20 +1,23 @@
 from ROOT import TFile, TH1F
 
 def tree_from_file(filename):
+    global fData
     fData = TFile(filename)
     data  = fData.Get("mini")
     return data
 
+def hist_lep_n(data):
+    hNumLeptons = TH1F("lep_n","Number of leptons",6,-0.5,5.5)
+    for i_event in range(1000):
+        data.GetEntry(i_event)
+        hNumLeptons.Fill(data.lep_n)
+    return hNumLeptons
+
 data = tree_from_file("/home/waugh/dimuon/data/mc_105987.WZ.root")
+hNumLeptons = hist_lep_n(data)
 
 nEvents = data.GetEntries()
 print("Number of events = "+str(nEvents))
-
-hNumLeptons = TH1F("lep_n","Number of leptons",6,-0.5,5.5)
-
-for i_event in range(1000):
-    data.GetEntry(i_event)
-    hNumLeptons.Fill(data.lep_n)
 
 hNumLeptons.Draw()
 
