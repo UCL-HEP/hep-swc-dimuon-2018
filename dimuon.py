8442efc13c8b83650d9e24dbe77cdd4bc1a58efa
Ben Waugh <b.waugh@ucl.ac.uk>
2018-11-09 11:52:54 +0000

    Print number of leptons in first event

--- dimuon.py
+++ dimuon.py
@@ -3,3 +3,6 @@ fData = ROOT.TFile("/home/waugh/dimuon/data/mc_105987.WZ.root")
 data  = fData.Get("mini")
 nEvents = data.GetEntries()
 print("Number of event = "+str(nEvents))
+
+data.GetEntry(0)
+print("Number of leptons in first event = "+str(data.lep_n))
