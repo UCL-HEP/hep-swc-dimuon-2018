3387f4523a4a31590063682d986433d2277c2a4a
Ben Waugh <b.waugh@ucl.ac.uk>
2018-11-09 12:08:05 +0000

    Display empty histogram and wait for keyboard input

--- dimuon.py
+++ dimuon.py
@@ -1,8 +1,14 @@
-import ROOT
-fData = ROOT.TFile("/home/waugh/dimuon/data/mc_105987.WZ.root")
+from ROOT import TFile, TH1F
+
+fData = TFile("/home/waugh/dimuon/data/mc_105987.WZ.root")
 data  = fData.Get("mini")
 nEvents = data.GetEntries()
 print("Number of event = "+str(nEvents))
 
 data.GetEntry(0)
 print("Number of leptons in first event = "+str(data.lep_n))
+
+hNumLeptons = TH1F("lep_n","Number of leptons",6,-0.5,5.5)
+hNumLeptons.Draw()
+
+raw_input("Press enter to exit.")
