c12b0bc06c18309bd1897e9fa53a4d050e452126
Ben Waugh <b.waugh@ucl.ac.uk>
2018-11-09 15:09:48 +0000

    Test no pairs from like sign particles

--- test_dimuon.py
+++ test_dimuon.py
@@ -9,3 +9,10 @@ def test_one_particle():
     particles = [Particle(None,1)]
     pairs = find_pairs(particles)
     assert len(pairs) == 0
+
+def test_two_like_sign():
+    p1 = Particle(None,1)
+    p2 = Particle(None,1)
+    particles = [p1,p2]
+    pairs = find_pairs(particles)
+    assert len(pairs) == 0
